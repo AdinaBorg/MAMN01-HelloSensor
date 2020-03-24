ded593fd60080c4ae7682b1b2f8cb9a131c79a43
AdinaBorg <ad4600bo-s@student.lu.se>
2020-03-24 14:02:39 +0100

    added buttons

---
+++ app/src/main/java/com/example/myfirstapp/DisplayAccelerometerActivity.java
@@ -0,0 +1,15 @@
+package com.example.myfirstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplayAccelerometerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_accelerometer);
+
+    }
+}
--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.myfirstapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps accelerometer button*/
+    public void getToAccelerometer(View view) {
+        Intent intent = new Intent(this, DisplayAccelerometerActivity.class);
+        startActivity(intent);
+    }
+
+    /** Called when the user taps compass button*/
+    public void getToCompass(View view) {
+        Intent intent = new Intent(this, DisplayCompassActivity.class);
+        startActivity(intent);
     }
 }
