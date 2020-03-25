b3dfae2f66e8552079ac904c0f56c54d753e2926
AdinaBorg <ad4600bo-s@student.lu.se>
2020-03-25 10:07:59 +0100

    accelerometer working, started on compass

--- app/src/main/java/com/example/myfirstapp/DisplayAccelerometerActivity.java
+++ app/src/main/java/com/example/myfirstapp/DisplayAccelerometerActivity.java
@@ -2,14 +2,51 @@ package com.example.myfirstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.widget.TextView;
 
-public class DisplayAccelerometerActivity extends AppCompatActivity {
+import java.text.DecimalFormat;
+
+public class DisplayAccelerometerActivity extends AppCompatActivity implements SensorEventListener {
+    private TextView xText, yText, zText;
+    private Sensor mySensor;
+    private SensorManager SM;
+    private static DecimalFormat df = new DecimalFormat("0.00");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_accelerometer);
 
+        //create Sensor Manager
+        SM = (SensorManager)getSystemService(SENSOR_SERVICE);
+
+        //Accelerometer Sensor
+        mySensor = SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        //Register sensor Listener
+        SM.registerListener(this, mySensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+        // Assign TextView
+        xText = (TextView)findViewById(R.id.xText);
+        yText = (TextView)findViewById(R.id.yText);
+        zText = (TextView)findViewById(R.id.zText);
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        xText.setText("X: " + df.format(event.values[0]));
+        yText.setText("Y: " + df.format(event.values[1]));
+        zText.setText("Z: " + df.format(event.values[2]));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        //not in use
     }
 }
